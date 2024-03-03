fc53e0f786989ebf8936fac9325184369400643b
Lipatnikova <aerovaya@gmail.com>
2024-03-03 13:30:39 +0400

    fix: upgrade urllib3

--- data/endpoints.py
+++ data/endpoints.py
@@ -6,7 +6,6 @@ This section contains url and endpoints
 class UrlAndEndPoints:
 
     BASE_URL = "http://localhost:8080/"
-    # BASE_URL = "http://37.113.157.167:9090/"
 
     ENTITIES = "api/getAll"
     ENTITY = f"api/get/"
